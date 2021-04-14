4b883398213c45eb9e09002f56312870fc6b6fec
Ana <nocillaftpizza@gmail.com>
2021-04-14 18:42:32 +0200

    changes 14/04

---
+++ New Unity Project/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float bulletspeed;
+    public GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3.MoveTowards(transform.position, enemy.transform.position, bulletspeed);
+        //transform.Translate(0, bulletspeed, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public NavMeshAgent agente;
+    public Vector3 posicion;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agente.SetDestination(posicion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
