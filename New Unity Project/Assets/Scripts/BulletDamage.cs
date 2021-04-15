1dc604dcc639d7e1dcc18ecc02756fab23c947f7
Ana <nocillaftpizza@gmail.com>
2021-04-15 14:49:30 +0200

    bullet damage and enemy health

---
+++ New Unity Project/Assets/Scripts/BulletDamage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) 
+        {
+            --collision.gameObject.GetComponent<EnemyLifes>().vidas; 
+        }
+
+        Destroy(gameObject);
+    }
+}
