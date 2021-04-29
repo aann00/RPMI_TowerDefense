029418fc82c6d52b419c0592ccd3e4caf2555180
Ana <nocillaftpizza@gmail.com>
2021-04-29 12:21:54 +0200

    autoshoot

--- New Unity Project/Assets/Scripts/TowerShoot.cs
+++ New Unity Project/Assets/Scripts/TowerShoot.cs
@@ -7,18 +7,42 @@ public class TowerShoot : MonoBehaviour
     public GameObject bulletPrefab;
     public GameObject enemy;
     public float height;
+    public float shootFrecuency;
+    private float lastShootTime;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (enemy != null) 
         {
-            GameObject bala = Instantiate(bulletPrefab, transform.position + new Vector3(0, height, 0), Quaternion.identity);
-            bala.GetComponent<BulletMovement>().enemy = enemy;
-        } 
+            if (Time.time - lastShootTime >= shootFrecuency)
+            {
+                //Realiza el disparo
+                GameObject bala = Instantiate(bulletPrefab, transform.position + new Vector3(0, height, 0), Quaternion.identity);
+                //Asigna la bala instanciada al enemigo
+                bala.GetComponent<BulletMovement>().enemy = enemy;
+                //Registra el momento en que se ha disparado
+                lastShootTime = Time.time;
+            }
+        }
+        
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy")) 
+        {
+            enemy = other.gameObject;
+        }
+    }
 
-
-
+    private void OnTriggerExit(Collider other)
+    {
+        enemy = null;
     }
 }
