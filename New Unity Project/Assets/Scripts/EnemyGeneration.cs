e6469cc1935d7a613678806fbf602e32d308eebc
Ana <nocillaftpizza@gmail.com>
2021-04-29 13:38:34 +0200

    29/04

---
+++ New Unity Project/Assets/Scripts/EnemyGeneration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGeneration : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float enemyFrecuency;
+    private float lastEnemyTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GenerateEnemy();
+    }
+
+    private void GenerateEnemy() 
+    {
+        if(Time.time - lastEnemyTime >= enemyFrecuency) 
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            lastEnemyTime = Time.time;
+        }
+        
+
+    }
+}
